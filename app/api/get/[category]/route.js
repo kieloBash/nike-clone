28818abdffbd35c89a6804da0ac23f0ab7baf216
Kielo Bash Mercado <kielo_bash_mercado@dlsu.edu.ph>
2023-05-15 21:58:25 +0800

    update bugs

--- app/api/get/[category]/route.js
+++ app/api/get/[category]/route.js
@@ -1,10 +1,11 @@
 import Item from "@/models/item";
 import { connectToDB } from "@/utils/database";
 
-export const GET = async (request) => {
+export const GET = async (request, {params}) => {
     try {
         await connectToDB()
         const items = await Item.find({category: "Shoes"});
+        // console.log(params.category)
         return new Response(JSON.stringify(items), { status: 200 })
     } catch (error) {
         return new Response("Failed to fetch all prompts", { status: 500 })
